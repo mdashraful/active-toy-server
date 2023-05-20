08103c9e27cf4f7dab85d19871cf9cb109a8fae9
mdashraful <mdashrafulislam369@gmail.com>
2023-05-20 19:46:17 +0600

    single toy added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -37,7 +37,14 @@ async function run() {
     });
 
     app.get("/toys", async (req, res) => {
-      const result = await toyCollection.find().toArray();
+      const result = await toyCollection.find().limit(20).toArray();
+      res.send(result);
+    });
+
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
       res.send(result);
     });
 
